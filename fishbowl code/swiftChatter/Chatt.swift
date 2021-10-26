2d416f242871cdab20a2ba21e05598e2300c68d8
Justine Qiu <44913699+jqiu08@users.noreply.github.com>
2021-10-26 12:02:15 -0400

    started implementing list

--- fishbowl code/swiftChatter/Chatt.swift
+++ fishbowl code/swiftChatter/Chatt.swift
@@ -9,7 +9,7 @@ import Foundation
 struct Chatt {
     var username: String?
     var message: String?
-    var timestamp: String?
+    var distance: String?
     
     @ChattPropWrapper var imageUrl: String?
     @ChattPropWrapper var videoUrl: String?
