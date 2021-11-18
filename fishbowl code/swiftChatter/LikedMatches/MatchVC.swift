c3e327af6ac18e12689c95e21e0955bc54635df5
sowgandhi11 <sowgandhi.bhattu11@gmail.com>
2021-11-18 11:28:50 -0500

    stuff for matches is working

--- fishbowl code/swiftChatter/LikedMatches/MatchVC.swift
+++ fishbowl code/swiftChatter/LikedMatches/MatchVC.swift
@@ -1,33 +1,104 @@
+////
+////  MatchVC.swift
+////  swiftChatter
+////
+////  Created by Sowgandhi Bhattu on 11/11/21.
+////
 //
-//  MatchVC.swift
-//  swiftChatter
 //
-//  Created by Sowgandhi Bhattu on 11/11/21.
+////
+////  MainVC.swift
+////  swiftChatter
+////
+////  Created by Rithika Ganesh on 9/7/21.
+////
 //
+//import Foundation
+//import UIKit
+//import AVKit
+//import SDWebImage
+//import CoreBluetooth
+//
+//final class MatchVC: UITableViewController {
+//
+//
+//
+//
+//    override func viewDidLoad() {
+//
+//        super.viewDidLoad()
+//        super.isModalInPresentation = true;
+//
+//        // setup refreshControler here later
+//             refreshControl?.addAction(UIAction(handler: refreshTimeline), for: UIControl.Event.valueChanged)
+//
+//             refreshTimeline(nil)
+//    }
+//
+//    // MARK:- TableView handlers
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // how many sections are in table
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // how many rows per section
+//        return FishbowlStore.shared.matches.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        // event handler when a cell is tapped
+//        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+//    }
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//           // populate a single cell
+//           guard let cell = tableView.dequeueReusableCell(withIdentifier: "MatchTableCell", for: indexPath) as? MatchTableCell else {
+//               fatalError("No reusable cell!")
+//           }
+//           print("deque works")
+//           let match = FishbowlStore.shared.matches[indexPath.row]
+//           cell.backgroundColor = (indexPath.row % 2 == 0) ? .systemGray5 : .systemGray6
+//           cell.usernameLabel.text = match.display_name
+//           print("cell username filled")
+//           if let urlString = match.imageUrl, let imageUrl = URL(string: urlString) {
+//                    cell.matchImageView.sd_setImage(with: imageUrl, placeholderImage: UIImage(systemName: "photo"), options: [.progressiveLoad])
+//                    cell.matchImageView.isHidden = false
+//                } else {
+//                    cell.matchImageView.image = nil
+//                    cell.matchImageView.isHidden = true
+//                }
+//           print("prior to return cell")
+//
+//           return cell
+//       }
+//    private func refreshTimeline(_ sender: UIAction?) {
+//        print("here")
+//            FishbowlStore.shared.getMatches { success in
+//                DispatchQueue.main.async {
+//                    if success {
+//                        print("succesfull went to get Matches")
+//                        self.tableView.reloadData()
+//                    }
+//                    // stop the refreshing animation upon completion:
+//                    self.refreshControl?.endRefreshing()
+//                }
+//
+//            }
+//        }
+//
+//}
 
 
-//
-//  MainVC.swift
-//  swiftChatter
-//
-//  Created by Rithika Ganesh on 9/7/21.
-//
 
-import Foundation
 import UIKit
 import AVKit
 import SDWebImage
-import CoreBluetooth
 
 final class MatchVC: UITableViewController {
- 
 
-
-    
     override func viewDidLoad() {
-    
         super.viewDidLoad()
-        super.isModalInPresentation = true;
         
         // setup refreshControler here later
              refreshControl?.addAction(UIAction(handler: refreshTimeline), for: UIControl.Event.valueChanged)
@@ -56,11 +127,12 @@ final class MatchVC: UITableViewController {
            guard let cell = tableView.dequeueReusableCell(withIdentifier: "MatchTableCell", for: indexPath) as? MatchTableCell else {
                fatalError("No reusable cell!")
            }
-           print("deque works")
+           
            let match = FishbowlStore.shared.matches[indexPath.row]
            cell.backgroundColor = (indexPath.row % 2 == 0) ? .systemGray5 : .systemGray6
            cell.usernameLabel.text = match.display_name
-           print("cell username filled")
+           
+        
            if let urlString = match.imageUrl, let imageUrl = URL(string: urlString) {
                     cell.matchImageView.sd_setImage(with: imageUrl, placeholderImage: UIImage(systemName: "photo"), options: [.progressiveLoad])
                     cell.matchImageView.isHidden = false
@@ -68,23 +140,18 @@ final class MatchVC: UITableViewController {
                     cell.matchImageView.image = nil
                     cell.matchImageView.isHidden = true
                 }
-           print("prior to return cell")
-       
+          
            return cell
        }
     private func refreshTimeline(_ sender: UIAction?) {
-        print("here")
             FishbowlStore.shared.getMatches { success in
                 DispatchQueue.main.async {
                     if success {
-                        print("succesfull went to get Matches")
                         self.tableView.reloadData()
                     }
                     // stop the refreshing animation upon completion:
                     self.refreshControl?.endRefreshing()
                 }
-                
             }
         }
-    
 }
