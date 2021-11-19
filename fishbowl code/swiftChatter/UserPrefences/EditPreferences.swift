bba5ce7a7f92a2a96a368dd6d915084717aba5a3
jennazch <68677660+jennazch@users.noreply.github.com>
2021-11-19 10:18:14 -0500

    frontend for user preferences

--- fishbowl code/swiftChatter/UserPrefences/EditPreferences.swift
+++ fishbowl code/swiftChatter/UserPrefences/EditPreferences.swift
@@ -16,16 +16,54 @@ final class EditPreferences: UIViewController {
         ChangesButton.layer.cornerRadius = 22
     }
     
+
+    @IBOutlet weak var relationshipPreferenceSerious: UIButton!
+    @IBOutlet weak var relationshipPreferenceCasual: UIButton!
+    @IBOutlet weak var genderPreferenceBoth: UIButton!
+    @IBOutlet weak var genderPreferenceMen: UIButton!
+    @IBOutlet weak var genderPreferenceWomen: UIButton!
+    var genderPreferenceChange = ""
+    var relationshipPreferenceChange = ""
+    
     @IBOutlet weak var ChangesButton: UIButton!
     
-   
+    @IBAction func IntWomen(_ sender: Any) {
+        genderPreferenceChange = "women"
+        genderPreferenceMen.tintColor = UIColor.blue
+        genderPreferenceWomen.tintColor = UIColor.systemPink
+        genderPreferenceBoth.tintColor = UIColor.blue
+    }
     
-
+    @IBAction func IntMen(_ sender: Any) {
+        genderPreferenceChange = "men"
+        genderPreferenceMen.tintColor = UIColor.systemPink
+        genderPreferenceWomen.tintColor = UIColor.blue
+        genderPreferenceBoth.tintColor = UIColor.blue
+    }
     
+    @IBAction func IntBoth(_ sender: Any) {
+        genderPreferenceChange = "both"
+        genderPreferenceMen.tintColor = UIColor.blue
+        genderPreferenceWomen.tintColor = UIColor.blue
+        genderPreferenceBoth.tintColor = UIColor.systemPink
+    }
+    
+    @IBAction func IntCasual(_ sender: Any) {
+        relationshipPreferenceChange = "casual"
+        relationshipPreferenceCasual.tintColor = UIColor.systemPink
+        relationshipPreferenceSerious.tintColor = UIColor.blue
+    }
+    
+    @IBAction func IntSerious(_ sender: Any) {
+        relationshipPreferenceChange = "serious"
+        relationshipPreferenceCasual.tintColor = UIColor.blue
+        relationshipPreferenceSerious.tintColor = UIColor.systemPink
+    }
     
     @IBAction func SaveChanges(_ sender: Any) {
         
         self.performSegue(withIdentifier: "GoBackToPreferences", sender: self)
+        // call post
     }
     
     
